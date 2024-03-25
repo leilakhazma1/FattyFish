897e6ab9ac296bc0394a3eb324f8d160dbe09940
leilakhazma1 <leilakhazma7@gmail.com>
2024-03-25 15:46:28 +1100

    save

--- FattyFish/src/App.jsx
+++ FattyFish/src/App.jsx
@@ -1,12 +1,11 @@
 import React from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
 import AppRoutes from './routes/AppRoutes';
 
 function App() {
   return (
-    <Router>
+   
       <AppRoutes />
-    </Router>
+   
   );
 }
 
