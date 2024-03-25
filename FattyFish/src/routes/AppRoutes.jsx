f97717c5824473cb9747550effa65d44f4245319
leilakhazma1 <leilakhazma7@gmail.com>
2024-03-25 16:02:18 +1100

    save

--- FattyFish/src/routes/AppRoutes.jsx
+++ FattyFish/src/routes/AppRoutes.jsx
@@ -3,12 +3,12 @@ import { Route, Routes } from 'react-router-dom';
 import Homepage from '../pages/Homepage';
 import BreakingFish from '../pages/BreakingFish';
 import RecipeIndex from '../pages/RecipeIndex';
-import NavBar from '../components/NavBar';
+
 
 function AppRoutes() {
   return (
     <>
-      <NavBar />
+     
       <Routes>
         <Route path="/" element={<Homepage />} />
         <Route path="/breakingfish" element={<BreakingFish />} />
