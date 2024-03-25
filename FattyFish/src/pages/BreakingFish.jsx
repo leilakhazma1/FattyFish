0552162a37f8a4ed887bce722b6db24a867e815a
leilakhazma1 <leilakhazma7@gmail.com>
2024-03-25 15:49:49 +1100

    save

---
+++ FattyFish/src/pages/BreakingFish.jsx
@@ -0,0 +1,12 @@
+
+import React from 'react';
+
+function BreakingFish() {
+  return (
+    <div className="BreakingFish">
+      <h1>Comprehensive Guide to Breaking Down Fish</h1>
+    </div>
+  );
+}
+
+export default BreakingFish;
--- FattyFish/src/routes/AppRoutes.jsx
+++ FattyFish/src/routes/AppRoutes.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import Homepage from '../pages/Homepage';
+import BreakingFish from '../pages/BreakingFish';
 
 function AppRoutes() {
   return (
     <Routes>
       <Route path="/" element={<Homepage />} />
+      <Route path="/" element={<BreakingFish />} />
     </Routes>
   );
 }
