1bb5d8087c479d9e135a9486aa4690481b84bb69
leilakhazma1 <leilakhazma7@gmail.com>
2024-03-26 15:35:38 +1100

    changes

--- FattyFish/src/App.jsx
+++ FattyFish/src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import AppRoutes from './routes/AppRoutes';
-import './App.css'; // Import the CSS file for styling
+import './Homepage.css'; // Import the CSS file for styling
 
 function App() {
   return (
