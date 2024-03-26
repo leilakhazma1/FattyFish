e255f22cff465eb2fb111caa31086bc3b3e3b630
leilakhazma1 <leilakhazma7@gmail.com>
2024-03-26 14:10:03 +1100

    basic styling

--- FattyFish/src/App.jsx
+++ FattyFish/src/App.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import AppRoutes from './routes/AppRoutes';
+import './App.css'; // Import the CSS file for styling
 
 function App() {
   return (
-   
+    <div className="app-container">
       <AppRoutes />
-   
+    </div>
   );
 }
 
--- FattyFish/src/routes/AppRoutes.jsx
+++ FattyFish/src/routes/AppRoutes.jsx
@@ -3,19 +3,18 @@ import { Route, Routes } from 'react-router-dom';
 import Homepage from '../pages/Homepage';
 import BreakingFish from '../pages/BreakingFish';
 import RecipeIndex from '../pages/RecipeIndex';
-import NavBar from '../components/NavBar';
+import NavBar from '../components /NavBar';
 
 function AppRoutes() {
   return (
-    <div className="app-container">
-      <h1 className="page-title">Fatty Fish</h1>
-      <NavBar />
+    <>
+     <NavBar />
       <Routes>
         <Route path="/" element={<Homepage />} />
         <Route path="/breakingfish" element={<BreakingFish />} />
         <Route path="/recipeindex" element={<RecipeIndex />} />
       </Routes>
-    </div>
+    </>
   );
 }
 
