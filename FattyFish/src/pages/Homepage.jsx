cbc6de858b47b18726aea0da4f9a6e838b9e41d8
leilakhazma1 <leilakhazma7@gmail.com>
2024-03-27 11:33:59 +1100

    background

--- FattyFish/src/pages/Homepage.jsx
+++ FattyFish/src/pages/Homepage.jsx
@@ -6,6 +6,11 @@ function Homepage() {
   return (
     <div className="Homepage">
    <h1> A Homage to the Fatty Fish</h1>
+   <p> A fish shaped loved letter; </p>
+   <p>  A guide on how to source, cook and eat fish  </p>
+
+  
+   
     </div>
   );
 }
