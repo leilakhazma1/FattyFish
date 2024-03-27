ce057ba8c4b7b5f418925dea6419153f71a61005
leilakhazma1 <leilakhazma7@gmail.com>
2024-03-28 10:49:31 +1100

    video

--- FattyFish/src/pages/BreakingFish.jsx
+++ FattyFish/src/pages/BreakingFish.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import '../styles/Home.css';
 import FishAnatomy from '../components /FishAnatomy';
 import coheranceImage from '../assets/nocoherance.png'; 
+import videoThumbnail from '../assets/red.png'
 
 
 function BreakingFish() {
@@ -11,19 +12,11 @@ function BreakingFish() {
       <FishAnatomy />
       <img src={coheranceImage} alt="Hat" className="coherance-image" />
       <div className="video-container">
-      <iframe
-          width="320"  
-          height="180" 
-          src="https://www.youtube.com/embed/GQJ0Qufe2So?start=45"
-          title="YouTube video player"
-          frameborder="0"
-          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-          allowFullScreen
-        ></iframe>
-        </div>
-    
+        <a href="https://www.youtube.com/embed/GQJ0Qufe2So?start=45" target="_blank" rel="noreferrer">
+          <img src={videoThumbnail} alt="Video Thumbnail" className="video-thumbnail" />
+        </a>
       </div>
-    
+    </div>
   );
 }
 
