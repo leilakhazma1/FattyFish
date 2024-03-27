83b259247da98e40a38e46aef554412203b64b2d
leilakhazma1 <leilakhazma7@gmail.com>
2024-03-28 10:41:09 +1100

    video

--- FattyFish/src/pages/BreakingFish.jsx
+++ FattyFish/src/pages/BreakingFish.jsx
@@ -3,12 +3,14 @@ import '../styles/Home.css';
 import FishAnatomy from '../components /FishAnatomy';
 import coheranceImage from '../assets/nocoherance.png'; 
 
+
 function BreakingFish() {
   return (
     <div className="BreakingFish">
       <h1>Comprehensive Guide to Breaking Down Fish</h1>
       <FishAnatomy />
       <img src={coheranceImage} alt="Hat" className="coherance-image" />
+      <div className="video-container">
       <iframe
           width="320"  
           height="180" 
@@ -16,8 +18,9 @@ function BreakingFish() {
           title="YouTube video player"
           frameborder="0"
           allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-          allowfullscreen
+          allowFullScreen
         ></iframe>
+        </div>
     
       </div>
     
