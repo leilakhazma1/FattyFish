aec70e626e1f7d3a363029f6e15bb46338840ee1
leilakhazma1 <leilakhazma7@gmail.com>
2024-03-27 13:37:18 +1100

    save

--- FattyFish/src/pages/RecipeIndex.jsx
+++ FattyFish/src/pages/RecipeIndex.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
-import RecipeList from '../components /RecipeList';
+import RecipeList from '../components/RecipeList';
 import '../styles/Home.css';
 
 function RecipeIndex() {
   return (
     <div className="RecipeIndex">
       <h1>Recipe Index</h1>
-      <RecipeList />
-
-      
+      <div className="RecipeListContainer">
+        <RecipeList />
+      </div>
     </div>
   );
 }
